409d253d3a751beba7e1aba526717f9e1025fdb6
cherryshneva <cherryshneva@gmail.com>
2023-11-29 16:45:30 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int a) {
+
+        int rublesPerMile = 20;
+        int miles = (a / rublesPerMile);
+
+        return miles;
+
+    }
+}
